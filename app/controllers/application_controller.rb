5a20d9aac335654ac9e25f03a9ada76a35e9c7f9
snazzafrass <snazzafrass@gmail.com>
2018-08-03 15:33:59 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,31 @@
 class ApplicationController < Sinatra::Base
 
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
+ configure do
+   set :public_folder, 'public'
+   set :views, 'app/views'
+ end
+ 
+   get '/' do
+   erb :charity_quiz
+ end
 
+get '/animal' do
+ erb :animals
+end
+
+
+post '/charity' do
+ @name = params[:username]
+ @answer = params[:q1]
+
+if @answer.to_i == 2
+   erb :animals
+ else
+   erb :human
+end
+end
+ 
+get '/charity' do
+erb :charity
+end
 end
--- app/models/result.rb
+++ app/models/result.rb
@@ -1,3 +1,1 @@
-class Result
 
-end
